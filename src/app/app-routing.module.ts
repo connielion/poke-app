725df43961d0aee0bbd41fbee671fdb6bdcc90fd
Connie <clai011@ucr.edu>
2018-08-10 03:21:41 -0700

    home done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 import { PokedexComponent } from './pokedex/pokedex.component';
-import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'pokedex', component: PokedexComponent},
-  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   hi = 'Hi! I\'ve integrated the RESTful Pokemon API from:';
+  ps = 'This was a great opportunity to experiment with Bootstrap 4 since I was taught Bootstrap 3, so I did. :P';
 
   ngOnInit() {
   }
