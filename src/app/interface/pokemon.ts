61107c613f15c40b5d038e5165c0a659957af4c8
Connie <clai011@ucr.edu>
2018-08-10 04:53:36 -0700

    interface done

--- src/app/interface/pokemon.ts
+++ src/app/interface/pokemon.ts
@@ -1,2 +1,16 @@
 export interface Pokemon {
+  id: number;
+  name: string;
+  height: number;
+  order: number;
+  abilities: [
+    {
+      is_hidden: true;
+      slot: number;
+      ability: {
+        name: string;
+        url: string;
+      }
+    }
+  ];
 }
